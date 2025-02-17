7633852972397601e5aec48be71ba2286204940e
davezigreat <yudave1999@gmail.com>
2025-02-17 16:58:58 +0800

    Registration Form

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Products, Order
+from django.contrib.auth.models import Group
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+    list_filter = ['category',]
 
 # Register your models here.
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Order)
+#admin.site.unregister(Group)
